3c919e46818f783fa5e6ddb853aab62f43f72400
Rizky Andyno Ramadhan <rizkydichi@gmail.com>
2022-04-13 16:32:50 +0700

    feat: add docker compose

--- app.js
+++ app.js
@@ -9,6 +9,15 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+// database setup
+var mongoose = require('mongoose');
+var mongoDB = process.env.MONGO_SERVER;
+console.log(mongoDB);
+
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
