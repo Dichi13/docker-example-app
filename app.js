e9679d9b3975433cac05b3d04cdffb29086aea2d
Rizky Andyno Ramadhan <rizkydichi@gmail.com>
2022-04-14 13:58:12 +0700

    Add functionality to the app

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
 
 var app = express();
 
 // database setup
 var mongoose = require('mongoose');
 var mongoDB = process.env.MONGO_SERVER;
-console.log(mongoDB);
 
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
@@ -30,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
